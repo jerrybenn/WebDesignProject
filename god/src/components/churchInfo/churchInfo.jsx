186c4b401dbe31f8e624c34213df4c8953c68159
Danielson A <adjocydanielson@gmail.com>
2025-03-11 14:19:56 -0400

    religous Icon

---
+++ god/src/components/churchInfo/churchInfo.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import './churchInfo.css';
+
+export default function churchInfo() {
+  return (
+    <Box className="rel-card-container"> {/* Flex container */}
+      {/* First Card */}
+      <Card className="rel-card">
+        <CardMedia component="img"
+         alt="St. Mary's Church" 
+         height="140" 
+         image="/RelCardImages/StMary.jpg" />
+
+        <CardContent>
+          <Typography gutterBottom variant="h5">St. Mary's Church</Typography>
+
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            St. Mary’s Catholic Church<br />
+            Assumption of the Blessed Virgin Mary <br />
+            119 South Prince Street, Lancaster, PA 17603<br />
+            (717) 392-2578
+          </Typography>
+
+        </CardContent>
+
+        <CardActions>
+          <Button size="small" 
+          component="a" href="https://stmaryslancaster.org/" 
+          target="_blank" rel="noopener noreferrer">
+            Website
+          </Button>
+        </CardActions>
+      </Card>
+
+    </Box>
+  );
+}
--- god/src/pages/homePage/Home.jsx
+++ god/src/pages/homePage/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css'
+import RelCard from "../../components/RelCard/RelCard";
+import Icon from '../../components/religionIcon/religionIcon';
 
 const Home = () => {
   return (
     <div className="homeContainer">
         <h1>Home</h1>
+        <RelCard/>
+        <Icon/>
     </div>
   )
 }
